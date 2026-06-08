5eb44de06f43645f8fa59e85f103292a511aa01d
Hardik Takkar <hardik.takkar@gmail.com>
2026-06-08 12:42:30 +0000

    datatypes summary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+// Primitive
+
+// 7 types : String, number, boolean, null, undefined, symbol,BigInt
+
+const score = 100
+const scoreValue = 100.4
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+//const bigNumber = 3456978950085n
+
+
+//const heros = ["spiderman","Batman","ironman"]
+//let myobj = { name:Hardik
+    age: 20
+//}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof myFunction);
+
+//reference(non primitive)
+
+// Array, Objects, Functions
+
