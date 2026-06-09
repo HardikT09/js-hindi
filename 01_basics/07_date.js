c0d7e6a0d6f64a5b2d570d8cdfb743a08a68641f
Hardik Takkar <hardik.takkar@gmail.com>
2026-06-09 07:54:41 +0000

    Date

---
+++ 01_basics/07_date.js
@@ -0,0 +1,31 @@
+let myDate = new Date()
+//console.log(myDate)
+
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+//console.log(typeof myDate);
+
+//let myCreatedDate = new Date(2026, 5, 9)
+//console.log(myCreatedDate.toDateString());
+let myCreatedDate = new Date("2023-01-14")
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+//console.log(myTimeStamp);
+
+//onsole.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+//console.log(newDate.getMonth());
+//console.log(newDate.getMonth() + 1);
+
+console.log(newDate.getFullYear());
+
+
+
+
+
+
