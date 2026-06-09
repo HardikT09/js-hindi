5effbb44fece2c73b1f3015995bcf62e5628b097
Hardik Takkar <hardik.takkar@gmail.com>
2026-06-09 06:21:27 +0000

    numbers

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,27 @@
+const score = 400
+
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString())
+console.log(balance.toString().length);  //gives lenght of the original no. which has been converted to string
+
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.5678
+console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString())
+
+console.log(hundreds.toLocaleString('en-IN'));
+
+//-------------------------------------------------------------
+
+
+
+
+
+
