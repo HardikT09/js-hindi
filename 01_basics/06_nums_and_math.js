6446d809173fbe67974a99ce5ebe42c1cf976818
Hardik Takkar <hardik.takkar@gmail.com>
2026-06-09 06:57:11 +0000

    maths

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,24 +1,55 @@
 const score = 400
 
-console.log(score);
+//console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+//console.log(balance);
 
-console.log(balance.toString())
-console.log(balance.toString().length);  //gives lenght of the original no. which has been converted to string
+//console.log(balance.toString())
+//console.log(balance.toString().length);  //gives lenght of the original no. which has been converted to string
 
-console.log(balance.toFixed(2));
+//console.log(balance.toFixed(2));
 
 const otherNumber = 23.5678
-console.log(otherNumber.toPrecision(2));
+//console.log(otherNumber.toPrecision(2));
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString())
+//console.log(hundreds.toLocaleString())
 
-console.log(hundreds.toLocaleString('en-IN'));
+//console.log(hundreds.toLocaleString('en-IN'));
 
-//-------------------------------------------------------------
+//----------------------------------------------------------------
+
+
+//console.log(Math);
+
+//console.log(Math.abs(-4));
+
+//console.log(Math.round(4.5));
+
+//console.log(Math.ceil(4.2));
+//console.log(Math.floor(4.2));
+
+//console.log(Math.min(4,5,9,6));
+//console.log(Math.max(4,5,8,9));
+
+//console.log(Math.random());
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max - min + 1))+ min);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
