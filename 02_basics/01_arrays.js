e3640d085ad5c261526cdc77140ffb7f87e29db2
Hardik Takkar <hardik.takkar@gmail.com>
2026-06-09 09:23:20 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,14 @@
+const myArr = [1 ,2 ,3 ,4, 5]
+const myHeros = ["shaktiman","naagraj"]
+
+const myArr2= new Array(1, 2, 3, 4)
+
+//console.log(myArr[0]);
+//console.log(myArr[3]);
+
+myArr.push(6)
+myArr.push(8)
+myArr.pop()
+console.log(myArr)
+
+
