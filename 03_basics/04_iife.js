38ec17915357f54cf2fdd852f7bcf17462b1c80d
Hardik Takkar <hardik.takkar@gmail.com>
2026-06-10 06:48:46 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+
+(function chai(){
+    console.log(`DB CONNECTED`);
+
+})();    //;(semicolon) needed to tell it to end it here and start next
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+    
+})("Hardik")
+
