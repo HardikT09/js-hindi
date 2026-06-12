e72c6677a9eb0d56fb94a701c8e3d247b7e74c5e
HardikT09 <hardik.takkar@gmail.com>
2026-06-12 17:50:53 +0530

    static properties

---
+++ 10_classes_oop/staticprop.js
@@ -0,0 +1,28 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `12345`
+    }
+}
+
+const hardik = new User("hardik")
+
+console.log(User.createId()) 
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+
+console.log(Teacher.createId()) 
