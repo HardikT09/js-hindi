8295dba437bc10d4c54d6538271f2a4111c86e93
HardikT09 <hardik.takkar@gmail.com>
2026-06-13 15:12:21 +0530

    getter setter

---
+++ 10_classes_oop/getter_setter.js
@@ -0,0 +1,24 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}hardik`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const hardik = new User("H@hardik.ai", "abc")
+console.log(hardik.email);
